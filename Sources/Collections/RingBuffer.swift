a830d85dcb0554dfc1722c828d2a6b40fc307c0f
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2024-10-31 13:29:04 -0400

    ring buffer count

--- Sources/Collections/RingBuffer.swift
+++ Sources/Collections/RingBuffer.swift
@@ -2,9 +2,12 @@ public struct RingBuffer<T> {
     public private(set) var buffer: [T]
     public private(set) var index: Int
 
+    public let count: Int
+
     public init(repeating value: T, count: Int) {
         buffer = [T](repeating: value, count: count)
         index = 0
+        self.count = count
     }
 
     public mutating func push(_ value: T) {
