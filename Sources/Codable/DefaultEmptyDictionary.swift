9b6f8fe401c3ca2a6d39c2b679234e9326f88b2e
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2025-02-22 21:47:16 -0500

    default codable yoink

---
+++ Sources/Codable/DefaultEmptyDictionary.swift
@@ -0,0 +1,9 @@
+public struct DefaultEmptyDictionaryStrategy<Key: Decodable & Hashable, Value: Decodable>: DefaultCodableStrategy {
+    public static var defaultValue: [Key: Value] { return [:] }
+}
+
+/// Decodes Dictionaries returning an empty dictionary instead of nil if applicable
+///
+/// `@DefaultEmptyDictionary` decodes Dictionaries and returns an empty dictionary instead of nil if the Decoder is unable
+/// to decode the container.
+public typealias DefaultEmptyDictionary<K, V> = DefaultCodable<DefaultEmptyDictionaryStrategy<K, V>> where K: Decodable & Hashable, V: Decodable
